86bd147d95175ab9be44e62b0f98f055847da06e
Anubhav Singh <jonsnower07@gmail.com>
2026-06-25 23:30:42 +0530

    fix: datasources -> datasource (singular) in prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: path.join("prisma", "schema.prisma"),
-  datasources: {
+  datasource: {
     db: {
       url: process.env.DATABASE_URL!,
     },
